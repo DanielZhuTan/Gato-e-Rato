6bcaeedd671232e7d00641f8c9be2b8e1044f3e6
EngenheiraFreitas <>
2021-11-09 10:09:05 -0300

    Projeto Corrigido

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
 var gato, gatoImg1, gatoImg2, gatoImg3;
-var rato, ratoImg1, ratoImg2;
+var rato, ratoImg1, ratoImg2, ratoImg3;
 var planodefundo, planodefundoImg;
 
 function preload() {
     //carregue as imagens aqui
-    planodefundoImg = loadImage("garden.png");
-    gatoImg1 = loadImage("cat1.png");
-    ratoImg1 = loadImage("mouse1.png");
-    gatoImg2 = loadImage("cat2.png","cat3.png");
-    ratoImg2 = loadImage("mouse2.png","mouse3.png");
-    gatoImg3 = loadImage("cat4.png");
+    planodefundoImg = loadImage("images/garde.png");
+    gatoImg1 = loadImage("images/cat1.png");
+    gatoImg2 = loadImage("images/cat2.png","images/cat3.png");
+    gatoImg3 = loadImage("images/cat4.png");
+    ratoImg1 = loadImage("images/mouse1.png");
+    ratoImg2 = loadImage("images/mouse2.png","images/mouse3.png");
+    ratoImg3 = loadAnimation("images/mouse4.png");
 }
 
 function setup(){
@@ -20,40 +21,45 @@ function setup(){
     gato.addAnimation("gatoSentado", gatoImg1);
     gato.scale = 0.2;
 
-    rato = createSprite(560, 600);
+    rato = createSprite(200, 600);
     rato.addAnimation("ratoQueijo", ratoImg1);
-    rato.scale = 0.2;
+    rato.scale = 0.15;
     
 }
 
 function draw() {
 
-    background(255);
+    background(planodefundoImg);
     //Escreva a condição aqui para avaliar se o gato e o rato colidem
 
     if(gato.x - rato.x < (gato.width - rato.width)/2){
         gato.velocityX = 0;
-        gato.addAnimation("últimaImageGato", catImg3);
-        gato.chargeAnimation("últimaImageGato");
+        gato.addAnimation("ultimaImageGato", catImg3);
         gato.x = 300;
         gato.scale = 0.2;
+        gato.chargeAnimation("ultimaImageGato");
+        rato.addAnimation("ratoproxima",ratoImg3);
+        rato.scale = 0.15;
+        rato.chargeAnimation("ratoproxima");
+        
     }
 
     drawSprites();
 }
 
 
-function keyPressed(){
+function mousePressed(){
 
   //Para mover e alterar a animação, escreva o código aqui
 
   if(keyCode === LEFT_ARROW){
-     rato.addAnimation("ratoProvocando", ratoImg2);
-     rato.chargeAnimation("ratoProvocando");
-     MouseEvent.frameDelay = 25;
+    gato.velocityX = -5;
+    gato.addAnimation("gatoCorrendo",gatoImg2);
+    gato.chargeAnimation("gatoCorrendo");
 
-     gato.velocityX = -5;
-
+    rato.addAnimation("ratoProvocando", ratoImg2);
+    rato.frameDelay = 25;
+    rato.chargeAnimation("ratoProvocando");
   }
 
 
